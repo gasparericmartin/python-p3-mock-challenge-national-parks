a07a53bdb60d7fe002045bbf41a2cb6e739ebcd3
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-04-24 13:14:12 -0400

    Complete assignment

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,22 +1,54 @@
+import re
+
 class NationalPark:
+
+    all = []
 
     def __init__(self, name):
         self.name = name
+
+        NationalPark.all.append(self)
+    
+    @property
+    def name(self):
+        return self._name 
+    
+    @name.setter
+    def name(self, name):
+        if not hasattr(self, 'name') and isinstance(name, str) and len(name) >= 3:
+            self._name = name
         
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if trip.national_park == self]
     
     def visitors(self):
-        pass
+        unique_list = []
+        total_list = [trip.visitor for trip in Trip.all if trip.national_park == self]
+
+        for visitor in total_list:
+            if visitor not in unique_list:
+                unique_list.append(visitor)
+        
+        return unique_list
     
     def total_visits(self):
-        pass
+        return len([trip for trip in Trip.all if trip.national_park == self])
     
     def best_visitor(self):
-        pass
+        best = None
+        # visitors = self.visitors()
 
-
+        for visitor in self.visitors():
+            if not best:
+                best = visitor
+            elif visitor.total_visits_at_park(self) > best.total_visits_at_park(self):
+                best = visitor
+        
+        return best
+            
 class Trip:
+
+    all = []
     
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
@@ -24,17 +56,82 @@ class Trip:
         self.start_date = start_date
         self.end_date = end_date
 
+        Trip.all.append(self)
+    
+    @property
+    def start_date(self):
+        return self._start_date
+    
+    @start_date.setter
+    def start_date(self, start_date):
+        pattern = r'[a-zA-z]+\s{1}[0-9]{1,2}[a-z]{2}'
+        regex = re.compile(pattern)
+
+        if  isinstance(start_date, str) and regex.match(start_date) and len(start_date) >= 7:
+            self._start_date = start_date
+    
+    @property
+    def end_date(self):
+        return self._end_date
+    
+    @end_date.setter
+    def end_date(self, end_date):
+        pattern = r'[A-z]+\s{1}[0-9]{1,2}[a-z]{2}'
+        regex = re.compile(pattern)
+
+        if isinstance(end_date, str) and regex.match(end_date) and len(end_date) >- 7:
+            self._end_date = end_date
+    
+    @property
+    def visitor(self):
+        return self._visitor
+    
+    @visitor.setter
+    def visitor(self, visitor):
+        self._visitor = visitor
+    
+    @property
+    def national_park(self):
+        return self._national_park
+    
+    @national_park.setter
+    def national_park(self, national_park):
+        self._national_park = national_park
+
+
+
 
 class Visitor:
 
+    all = []
+
     def __init__(self, name):
         self.name = name
+
+        Visitor.all.append(self)
+    
+    @property
+    def name(self):
+        return self._name 
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name
         
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if trip.visitor == self]
     
     def national_parks(self):
-        pass
+        unique_list = []
+        total_list = [trip.national_park for trip in Trip.all if trip.visitor == self]
+
+        for nat_park in total_list:
+            if nat_park not in unique_list:
+                unique_list.append(nat_park)
+        
+        return unique_list
     
     def total_visits_at_park(self, park):
-        pass
+        print(len([trip for trip in Trip.all if trip.visitor == self and trip.national_park == park]))
+        return len([trip for trip in Trip.all if trip.visitor == self and trip.national_park == park])
--- lib/debug.py
+++ lib/debug.py
@@ -6,6 +6,11 @@ from classes.many_to_many import Visitor
 from classes.many_to_many import Trip
 
 if __name__ == '__main__':
-    print("HELLO! :) let's debug :vibing_potato:")
+    p_1 = NationalPark("Yosemite")
+    vis_1 = Visitor("Tom")
+    vis_2 = Visitor("Mark")
+    trip1 = Trip(vis_1, p_1, "May 5th", "May 9th")
+    trip2 = Trip(vis_1, p_1, "January 5th", "January 20th")
+    trip3 = Trip(vis_2, p_1, "January 5th", "January 20th")
 
     ipdb.set_trace()
